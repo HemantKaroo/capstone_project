8e593d76fe15525bc94dde16f9a2ea59fa317a1e
Hemant Karoo <88471296+HemantKaroo@users.noreply.github.com>
2025-04-17 17:31:49 +0530

    Update CapstoneProjectApplication.java

--- capstone_project/src/main/java/com/elasticpath/ecommerce/CapstoneProjectApplication.java
+++ capstone_project/src/main/java/com/elasticpath/ecommerce/CapstoneProjectApplication.java
@@ -11,9 +11,16 @@ public class CapstoneProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneProjectApplication.class, args);
 	}
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+	String secret = "4SYypzLLiTSGoeLZAob498yIK3y2PoARMtpyaxgHzu";
+
+        String token = Jwts.builder()
+                .setSubject("test-user")
+                .claim("scope", "read write")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hour
+                .signWith(SignatureAlgorithm.HS256, secret.getBytes())
+                .compact();
+
+        System.out.println("Bearer " + token);
 
 }
